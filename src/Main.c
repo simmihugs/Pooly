296b309d9173b82779986503de892d4912766c26
simmihugs <simon.graetz@gmx.de>
2025-11-05 17:52:40 +0100

    Changed from fixed, stack allocated pool to dynamic heap pool

--- src/Main.c
+++ src/Main.c
@@ -10,14 +10,39 @@ typedef struct Block {
 
 typedef struct MemoryPool {
     Block *freeList;
-    unsigned char pool[POOL_SIZE];
+    unsigned char* pool;
+    size_t pool_size;
 } MemoryPool;
+
+MemoryPool *createMemoryPool(size_t pool_size) {
+    unsigned char* pool = malloc(sizeof(unsigned char) * pool_size);
+    if (pool == NULL) {
+        return NULL;
+    }
+
+    MemoryPool *mpool = malloc(sizeof(MemoryPool));
+    mpool->freeList = NULL;
+    mpool->pool = pool;
+    mpool->pool_size = pool_size;
+
+    return mpool;
+}
+
+void freeMemoryPool(MemoryPool *mpool) {
+    if (mpool) {
+        free(mpool->pool);
+        free(mpool);
+        printf("Freed memory pool\n");
+    }
+}
+
 
 void initMemoryPool(MemoryPool *pool) {
     pool->freeList = (Block *)pool->pool;
     Block *current = pool->freeList;
+    size_t block_count = pool->pool_size / sizeof(Block);
 
-    for (int i=0; i<(POOL_SIZE / sizeof(Block))-1; i++) {
+    for (int i=0; i<block_count-1; i++) {
         current->next = (Block *)((unsigned char *)current + sizeof(Block));
         current = current->next;
     }
@@ -42,17 +67,17 @@ void freeMemory(MemoryPool *pool, void *ptr) {
 }
 
 int main() {
-    MemoryPool pool;
-    initMemoryPool(&pool);
-    void *block1 = allocateMemory(&pool);
-    void *block2 = allocateMemory(&pool);
+    MemoryPool *pool = createMemoryPool(POOL_SIZE);
+    initMemoryPool(pool);
+    void *block1 = allocateMemory(pool);
+    void *block2 = allocateMemory(pool);
     printf("Allocated block1: %p\n", block1);
     printf("Allocated block2: %p\n", block2);
 
     // Free the blocks
-    freeMemory(&pool, block1);
-    freeMemory(&pool, block2);
+    freeMemory(pool, block1);
+    freeMemory(pool, block2);
     printf("Freed block1 and block2\n");
-    
+    freeMemoryPool(pool);
     return 0;
 }
